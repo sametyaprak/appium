a5863bf9757ec274533a9bc45f883252b56abf2d
sametyaprak <67242005+sametyaprak@users.noreply.github.com>
2020-11-27 12:17:30 +0100

    first commit

---
+++ src/test/java/com/automation/FirstAppiumTest.java
@@ -0,0 +1,32 @@
+package com.automation;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import org.junit.Test;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class FirstAppiumTest {
+
+    @Test
+    public void test1() throws MalformedURLException, InterruptedException {
+
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+
+        desiredCapabilities.setCapability("platformName","Android");
+        desiredCapabilities.setCapability("platformVersion","7.0");
+        desiredCapabilities.setCapability("deviceName","Pixel_2");
+        desiredCapabilities.setCapability("automationName","UiAutomator2");
+        desiredCapabilities.setCapability("app", "C:\\Users\\samet\\IdeaProjects\\AppiumAutomation\\etsy.apk");
+
+        AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(new URL("http://localhost:4723/wd/hub"),desiredCapabilities);
+
+        Thread.sleep(20000);
+
+        WebElement getStarted = driver.findElementById("com.etsy.android:id/btn_link");
+        getStarted.click();
+    }
+}
