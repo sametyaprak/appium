7e0e49303cc27f3d7d7a6641694326727a8edfd0
sametyaprak <67242005+sametyaprak@users.noreply.github.com>
2020-11-27 19:37:43 +0100

    appium ilk gun sonu

--- src/test/java/com/automation/EmulatorTest.java
+++ src/test/java/com/automation/EmulatorTest.java
@@ -4,20 +4,17 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.sql.Driver;
 
-public class FirstAppiumTest {
+public class EmulatorTest {
+
 
 
     @Test
-    public void test1() throws MalformedURLException, InterruptedException {
-
+    public void test1() throws InterruptedException, MalformedURLException {
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
 
         desiredCapabilities.setCapability("platformName","Android");
@@ -29,18 +26,8 @@ public class FirstAppiumTest {
 
 
         AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(new URL("http://localhost:4723/wd/hub"),desiredCapabilities);
-        //Actions actions = new Actions(driver);
-        Thread.sleep(20000);
 
-        WebElement darkModeCancel = driver.findElement(By.id("com.etsy.android:id/dark_mode_alert_dismiss"));
-        darkModeCancel.click();
-        WebElement getStarted = driver.findElement(By.id("com.etsy.android:id/btn_link"));
-        getStarted.click();
-        Thread.sleep(5000);
-        WebElement emailTextBox = driver.findElement(By.id("com.etsy.android:id/edit_username"));
-        emailTextBox.sendKeys("aliveli");
-        WebElement paswordTextBox = driver.findElement(By.id("com.etsy.android:id/edit_password"));
-        paswordTextBox.sendKeys("954625 ");
+        Thread.sleep(20000);
 
     }
 }
